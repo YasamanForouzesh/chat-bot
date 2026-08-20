506128c2f6b21c4251d03aed3602febf2ae4db6b
Yasaman Forouzesh <yasamanforouzesh@Yasis-MBP.lan>
2026-08-19 22:02:45 -0700

    fix call

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from llm_adapter.helper import llm_helper
-
-
+from dotenv import load_dotenv
+load_dotenv()
 def chat():
     llm = llm_helper("gpt-4.1-nano", "openai")
-    return llm.generate("hello tell one small and quick joke for who immigrate from Iran to USA to undrestand LA culcutre.")
+    rsp = llm.generate("hello tell one small and quick joke for who immigrate from Iran to USA to undrestand LA culcutre.")
+    return rsp.output_text
 
 
 def main():
